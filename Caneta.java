b1f4319057f7157f659da5a5845acbc7e3290745
Gabriel Fantin <fantinee@live.com>
2026-09-24 11:02:55 -0300

    Create Caneta.java

---
+++ Caneta.java
@@ -0,0 +1,30 @@
+package aula02;
+public class Caneta{
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status(){
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Está tampada?" + this.tampada);
+        System.out.println("A ponta é " + this.ponta);
+        System.out.println("A carga é " + this.carga);
+        System.out.println("O modelo é " + this.modelo);
+    }
+    void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+    void tampar(){
+        this.tampada = true;
+    }
+    void destampar(){
+        this.tampada = false;
+    }
+
+
+}
